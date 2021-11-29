f89853f12a29374904ccd248f5b9af611fe29e37
gabij <gabi.jospin@gmail.com>
2021-11-29 15:49:05 -0300

    transformando HOg numa funçaõ

--- Codigo/main.py
+++ Codigo/main.py
@@ -7,25 +7,28 @@ from Codigo.readFile import readFile
 def print_hi(name):
     print(f'Hello there \nGeneral {name}!!')
 
+def preProcessHog():
+    image = []
+    i = 0
+    for file_path in ipp.constants.file_path:
+        rf = readFile(file_path, (i % 2 == 0))
+        rf.generate()
+        print(rf.files)
+
+        for file in rf.files.itertuples():
+            image.append([f"{file.Person}_{str(file.img).zfill(4)}", i])
+
+        i += 1
+
+    # print(image)
+
+    for img in image:
+        imageF = ipp.image(img[0], test=(img[1] > 1))
+        hog = ipp.hog(imageF, test=(img[1] > 1))
+        hog.generate()
+
 
 if __name__ == '__main__':
     print_hi('Kenobi')
 
-image = []
-i = 0
-for file_path in ipp.constants.file_path:
-    rf = readFile(file_path, (i % 2 == 0))
-    rf.generate()
-    print(rf.files)
-
-    for file in rf.files.itertuples():
-        image.append([f"{file.Person}_{str(file.img).zfill(4)}", i])
-
-    i += 1
-
-# print(image)
-
-for img in image:
-    imageF = ipp.image(img[0], test=(img[1] > 1))
-    hog = ipp.hog(imageF,  test=(img[1] > 1))
-    hog.generate()
+# preProcessHog()
