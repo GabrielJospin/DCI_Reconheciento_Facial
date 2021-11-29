7f6cf9a8b23d6390645ed07d9d6faf3b2bf0f212
gabij <gabi.jospin@gmail.com>
2021-11-29 11:55:58 -0300

    Convert all images from a .txt file

--- Codigo/main.py
+++ Codigo/main.py
@@ -1,5 +1,5 @@
 import imagePreProcessing as ipp
-import os
+import pandas as pd
 
 
 def print_hi(name):
@@ -9,6 +9,32 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Kenobi')
 
-image = ipp.image("Bill_Gates_0001")
-hog = ipp.hog(image)
-hog.generate()
+pairs = pd.DataFrame(columns=['Person', 'img'])
+
+file_path = ipp.constants.TRAINING_PATH + "/pairsDevTrain" + ".txt"
+
+with open(file_path, "rb") as infile:
+    DBlen = int(infile.readline())
+    while True:
+        text = infile.readline()
+
+        if not text: break
+        (person, img1, img2) = text.split()
+        person = person.decode('UTF-8')
+        img1 = int(img1)
+        img2 = int(img2)
+        pairs = pairs.append({'Person': person, 'img': img1}, ignore_index=True)
+        pairs = pairs.append({'Person': person, 'img': img2}, ignore_index=True)
+
+print(pairs)
+image = []
+
+for pair in pairs.itertuples():
+    image.append(f"{pair.Person}_{str(pair.img).zfill(4)}")
+
+print(image)
+
+for img in image:
+    imageF = ipp.image(img)
+    hog = ipp.hog(imageF)
+    hog.generate()
