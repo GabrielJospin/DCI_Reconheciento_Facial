1ed4f39c7789de5b5e2e76523bc2235ab1566f6f
gabij <gabi.jospin@gmail.com>
2022-03-09 15:42:13 -0300

    finsl version

--- Codigo/main.py
+++ Codigo/main.py
@@ -74,85 +74,83 @@ def oper(ValuesIn, ValuesOut, test, ope, modelo):
 if __name__ == '__main__':
     print_hi('Kenobi')
 
-print(f'---------------------------------CNN----------------------------------------')
-# preProcessCNN()
 print(f'----------------------------------HOG---------------------------------------')
-# preProcessHog()
+preProcessHog()
 print(f'------------------------------------LBP-------------------------------------')
-# preProcessLbp()
+preProcessLbp()
 print(f'-------------------------------------------------------------------------')
 modelos = ['lbp', 'hog']
-# for modelo in modelos:
-#
-#     print(f"---------------- modelo:{modelo} --------------------------- ")
-#
-#     df = clf.dataFrame(constants.file_path[0], True, modelo)
-#     df2 = clf.dataFrame(constants.file_path[1], False, modelo)
-#
-#     dfTest = clf.dataFrame(constants.file_path[2], True, modelo, test=True)
-#     df2Test = clf.dataFrame(constants.file_path[3], False, modelo, test=True)
-#
-#     gerateDF(df)
-#     gerateDF(df2)
-#     gerateDF(dfTest)
-#     gerateDF(df2Test)
-#
-#     (X1, Y1) = df.getDB()
-#     (X2, Y2) = df2.getDB()
-#     (Xt1, Yt1) = dfTest.getDB()
-#     (Xt2, Yt2) = df2Test.getDB()
-#
-#     X = X1.append(X2, ignore_index=True)
-#     Y = Y1.append(Y2, ignore_index=True)
-#
-#     Xt = Xt1.append(Xt2, ignore_index=True)
-#     Yt = Yt1.append(Yt2, ignore_index=True)
-#     Yt = np.asarray(Yt)
-#
-#     # print(X)
-#     # print(Y)
-#     # print(Xt)
-#     # print(Yt)
-#
-#     newX = pd.DataFrame(X)
-#     newXt = pd.DataFrame(Xt)
-#     newY = pd.DataFrame(Y)
-#     newYt = np.asarray(Yt)
-#
-#     classificadores = [clf.mlp, clf.svm]
-#
-#     for ope in classificadores:
-#
-#         print(f"---------------- class:{ope.__name__} --------------------------- ")
-#         erro = 0
-#         outs = []
-#         Y = pd.DataFrame(oper(newX, newY, newXt, ope, modelo))
-#
-#         for index, x in Y.iterrows():
-#             if x[0] > 0.5:
-#                 out = 1
-#             else:
-#                 out = 0
-#
-#             outs.append(out)
-#             if newYt[index] != out:
-#                 erro += 1
-#
-#         error = pd.DataFrame(Y.__sub__(newYt))
-#         error.to_csv(f'{con.EXECUCAO_PATH}/exit/{modelo}.{ope.__name__}.error.csv')
-#
-#         print(np.power(error, 2).sum() / len(error))
-#         print((1 - erro / len(newYt)) * 100)
+for modelo in modelos:
+
+    print(f"---------------- modelo:{modelo} --------------------------- ")
+
+    df = clf.dataFrame(constants.file_path[0], True, modelo)
+    df2 = clf.dataFrame(constants.file_path[1], False, modelo)
+
+    dfTest = clf.dataFrame(constants.file_path[2], True, modelo, test=True)
+    df2Test = clf.dataFrame(constants.file_path[3], False, modelo, test=True)
+
+    gerateDF(df)
+    gerateDF(df2)
+    gerateDF(dfTest)
+    gerateDF(df2Test)
+
+    (X1, Y1) = df.getDB()
+    (X2, Y2) = df2.getDB()
+    (Xt1, Yt1) = dfTest.getDB()
+    (Xt2, Yt2) = df2Test.getDB()
+
+    X = X1.append(X2, ignore_index=True)
+    Y = Y1.append(Y2, ignore_index=True)
+
+    Xt = Xt1.append(Xt2, ignore_index=True)
+    Yt = Yt1.append(Yt2, ignore_index=True)
+    Yt = np.asarray(Yt)
+
+    # print(X)
+    # print(Y)
+    # print(Xt)
+    # print(Yt)
+
+    newX = pd.DataFrame(X)
+    newXt = pd.DataFrame(Xt)
+    newY = pd.DataFrame(Y)
+    newYt = np.asarray(Yt)
+
+    classificadores = [clf.mlp, clf.svm]
+
+    for ope in classificadores:
+
+        print(f"---------------- class:{ope.__name__} --------------------------- ")
+        erro = 0
+        outs = []
+        Y = pd.DataFrame(oper(newX, newY, newXt, ope, modelo))
+
+        for index, x in Y.iterrows():
+            if x[0] > 0.5:
+                out = 1
+            else:
+                out = 0
+
+            outs.append(out)
+            if newYt[index] != out:
+                erro += 1
+
+        error = pd.DataFrame(Y.__sub__(newYt))
+        error.to_csv(f'{con.EXECUCAO_PATH}/exit/{modelo}.{ope.__name__}.error.csv')
+
+        print(np.power(error, 2).sum() / len(error))
+        print((1 - erro / len(newYt)) * 100)
 
 df = clf.dataFrame(constants.file_path[0], True, 'cnn')
 df2 = clf.dataFrame(constants.file_path[1], False, 'cnn')
 dfTest = clf.dataFrame(constants.file_path[2], True, 'cnn', test=True)
 df2Test = clf.dataFrame(constants.file_path[3], False, 'cnn', test=True)
 
-# gerateDF(df)
-# gerateDF(df2)
-# gerateDF(dfTest)
-# gerateDF(df2Test)
+gerateDF(df)
+gerateDF(df2)
+gerateDF(dfTest)
+gerateDF(df2Test)
 
 (X1, Y1) = df.getDB()
 (X2, Y2) = df2.getDB()
